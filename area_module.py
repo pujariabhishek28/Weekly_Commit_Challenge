f8760054d155ad5341255b06d18da00ba044f3a9
pujariabhishek28 <pujariabhishek870@gmail.com>
2026-01-04 19:38:51 +0530

    Python Code

---
+++ area_module.py
@@ -0,0 +1,12 @@
+
+def rectangle_area(length, breadth):
+    return length * breadth
+
+def circle_area(radius):
+    return 3.14 * radius * radius
+
+def square_area(side):
+    return side * side
+
+def triangle_area(base, height):
+    return 0.5 * base * height
---
+++ main.py
@@ -0,0 +1,6 @@
+import area_module
+
+print("Rectangle Area:", area_module.rectangle_area(10, 5))
+print("Circle Area:", area_module.circle_area(7))
+print("Square Area:", area_module.square_area(4))
+print("Triangle Area:", area_module.triangle_area(6, 8))
